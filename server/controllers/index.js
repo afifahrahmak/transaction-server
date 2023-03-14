40773926b4d7ec755a6de2761ae44797195a920c
afifahrahmak <afifah.ark@gmail.com>
2023-03-14 16:35:07 +0700

    test

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -10,9 +10,9 @@ module.exports = {
 		try {
 			const { orderId } = req.params
 			const order = await Order.findByPk(orderId, { include: Product, transaction: t })
-			if (!order) throw { status: 404, msg: 'Order not found' }
-			if (order.isPaid) throw { status: 400, msg: 'Order has already been paid' }
-			if (order.Product.stock < order.quantity) throw { status: 400, msg: 'Product stock not available' }
+			if (!order) throw { msg: 'Order not found' }
+			if (order.isPaid) throw { msg: 'Order has already been paid' }
+			if (order.Product.stock < order.quantity) throw { msg: 'Product stock not available' }
 
 			await Order.update(
 				{ isPaid: true, paidDate: new Date() },
@@ -22,12 +22,9 @@ module.exports = {
 			await t.commit()
 			res.status(200).json({ msg: `Pay order with ID ${orderId} success` });
 		} catch (error) {
+			console.log(error);
 			await t.rollback()
-			if (error.status) {
-				res.status(error.status).json(error.msg)
-			} else {
-				res.status(500).json({ msg: 'Internal Server Error' })
-			}
+			res.status(500).json(error)
 		}
 	}
 };
